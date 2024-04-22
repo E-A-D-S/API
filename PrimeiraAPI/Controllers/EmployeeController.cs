7f011d9966fa084368929f2848988e3b773e5ca2
Eduardo <Eduardoeko7@gmail.com>
2024-04-22 03:00:19 -0300

    Começando novo projeto

---
+++ PrimeiraAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraAPI.Model;
+using PrimeiraAPI.ViewModel;
+using System;
+
+namespace PrimeiraAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/employee")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository ?? throw new ArgumentNullException(nameof(employeeRepository));
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromForm] EmployeeViewModel employeeView)
+
+        {
+            var filePath = Path.Combine("storage", employeeView.Photo.FileName);
+
+            var employee = new Employee(employeeView.Name, employeeView.Age, filePath);
+
+            using Stream fileStream = new FileStream(filePath, FileMode.Create);
+            employeeView.Photo.CopyTo(fileStream);
+
+            _employeeRepository.Add(employee);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("{id}/download")]
+
+        public IActionResult DonwloadPhoto(int id)
+        {
+            var employee = _employeeRepository.Get(id);
+
+            var dataBytes = System.IO.File.ReadAllBytes(employee.photo);
+
+            return File(dataBytes, "image/png");
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employees = _employeeRepository.Get();
+            return Ok(employees);
+        }
+    }
+}
---
+++ PrimeiraAPI/Data/EmployeeRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PrimeiraAPI.Model;
+
+namespace PrimeiraAPI.Data
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ConnectionContext _context;
+
+        public EmployeeRepository(ConnectionContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+        }
+
+        public List<Employee> Get()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public Employee? Get(int id)
+        {
+            return _context.Employees.Find(id);
+        }
+    }
+}
